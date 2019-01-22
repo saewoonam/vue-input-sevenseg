8784e3b612456b6e8508b94fa31e319606c4b888
Nam <nams@686swnmba.local>
2019-01-22 10:12:12 -0700

    modify so that is an example of how to use vue-input-sevenseg

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <div id="app"><v-input-sevenseg init=1.234567></v-input-sevenseg></div>
+  <div id="app">
+    <v-input-sevenseg v-model="value1"></v-input-sevenseg>
+    <br />
+    <p>value1: {{ value1 }}</p>
+    <br />
+    <v-input-sevenseg v-model="value2" digits=10 height=60 slant=25 />
+    <br />
+    <p>value2: {{ value2 }}</p>
+    <v-input-sevenseg v-model="value3"
+      digits=4
+      precision=2
+      @change="logChange(value3)"
+    >
+    </v-input-sevenseg>
+    <br />
+    <p>value3: {{ value3 }}</p>
+  </div>
 </template>
 
 <script>
@@ -9,6 +25,18 @@ export default {
   name: "app",
   components: {
     VInputSevenseg
+  },
+  data() {
+    return {
+      value1: 1.2,
+      value2: "0.123456789",
+      value3: 0.345
+    };
+  },
+  methods: {
+    logChange: function(value) {
+      console.log("logChange:", value);
+    }
   }
 };
 </script>
